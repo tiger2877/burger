45795e9e05e72f60f1e0f0beb103528048c2b03f
Pryambudhi Cahyadi <pcahyadi@gmail.com>
2019-03-03 19:24:55 -0500

    i made some changes

--- config/connection.js
+++ config/connection.js
@@ -2,14 +2,14 @@
 var mysql = require("mysql");
 var connection;
 
-if(process.env.JAWSDB_URL){
+if (process.env.JAWSDB_URL){
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
   connection = mysql.createConnection({
     port: 3306,
     host: "localhost",
     user: "root",
-    password: "",
+    password: "hacktheplanet",
     database: "burgers_db"
   });
 };
